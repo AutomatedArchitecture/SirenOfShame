9943ea0ef84bd838cb1c1e79d06f56064440cdd2
lprichar <none@none>
2011-08-26 09:47:01 -0400

    Fixed password encryption with special chars

--- SirenOfShame.Lib/Settings/TripleDESStringEncryptor.cs
+++ SirenOfShame.Lib/Settings/TripleDESStringEncryptor.cs
@@ -5,7 +5,8 @@ using System.Text;
 
 namespace SirenOfShame.Lib.Settings
 {
-    public class TripleDESStringEncryptor {
+    public class TripleDESStringEncryptor
+    {
         private byte[] _key;
         private byte[] _iv;
 
@@ -18,22 +19,34 @@ namespace SirenOfShame.Lib.Settings
             _provider = new TripleDESCryptoServiceProvider();
         }
 
-        public string EncryptString(string plainText)
+        public string EncryptString(string text)
         {
-            return Transform(plainText, _provider.CreateEncryptor(_key, _iv));
-        }
-        
-        public string DecryptString(string encryptedText)
-        {
-            return Transform(encryptedText, _provider.CreateDecryptor(_key, _iv));
-        }
-        
-        private string Transform(string text, ICryptoTransform transform)
-        {
+            ICryptoTransform transform = _provider.CreateEncryptor(_key, _iv);
+
             if (text == null)
             {
                 return null;
-            }        
+            }
+            using (MemoryStream stream = new MemoryStream())
+            {
+                using (CryptoStream cryptoStream = new CryptoStream(stream, transform, CryptoStreamMode.Write))
+                {
+                    byte[] input = Encoding.UTF8.GetBytes(text);
+                    cryptoStream.Write(input, 0, input.Length);
+                    cryptoStream.FlushFinalBlock();
+                    return Convert.ToBase64String(stream.ToArray());
+                }
+            }
+        }
+
+        public string DecryptString(string text)
+        {
+            ICryptoTransform transform = _provider.CreateDecryptor(_key, _iv);
+
+            if (text == null)
+            {
+                return null;
+            }
             using (MemoryStream stream = new MemoryStream())
             {
                 using (CryptoStream cryptoStream = new CryptoStream(stream, transform, CryptoStreamMode.Write))
@@ -41,8 +54,9 @@ namespace SirenOfShame.Lib.Settings
                     byte[] input = Convert.FromBase64String(text);
                     cryptoStream.Write(input, 0, input.Length);
                     cryptoStream.FlushFinalBlock();
-                    return Convert.ToBase64String(stream.ToArray());
+                    return Encoding.UTF8.GetString(stream.ToArray());
                 }
             }
-        }}
+        }
+    }
 }
---
+++ SirenOfShame.Test.Unit/Settings/TripleDESStringEncryptorTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SirenOfShame.Lib.Settings;
+
+namespace SirenOfShame.Test.Unit.Settings
+{
+    [TestClass]
+    public class TripleDESStringEncryptorTest
+    {
+        [TestMethod]
+        public void Simple()
+        {
+            var encryptor = new TripleDESStringEncryptor();
+            var encrypted = encryptor.EncryptString("password");
+            var decrypted = encryptor.DecryptString(encrypted);
+            Assert.AreEqual("password", decrypted);
+        }
+        
+        [TestMethod]
+        public void UnusualChars()
+        {
+            var encryptor = new TripleDESStringEncryptor();
+            var encrypted = encryptor.EncryptString("!@#$%^&*()- +=");
+            var decrypted = encryptor.DecryptString(encrypted);
+            Assert.AreEqual("!@#$%^&*()- +=", decrypted);
+        }
+    }
+}
