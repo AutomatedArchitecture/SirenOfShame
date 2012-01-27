19f01ae3c8ad1521ea775e49d2d37c812dc7d6d5
lprichar <none@none>
2012-01-26 22:39:57 -0500

    Updated bunk test

--- SirenOfShame.Test.Unit/CIEntryPointBuildStatus/HudsonBuildStatusTest.cs
+++ SirenOfShame.Test.Unit/CIEntryPointBuildStatus/HudsonBuildStatusTest.cs
@@ -15,11 +15,11 @@ namespace SirenOfShame.Test.Unit.CiEntryPointBuildStatus
         {
             var jenkinsBuildStatusForIssue10 = ResourceManager.JenkinsPassingBuild;
             BuildDefinitionSetting buildDefinitionSetting = new BuildDefinitionSetting();
+            buildDefinitionSetting.Name = "Name";
+            buildDefinitionSetting.Id = "BuildDefinitionId";
             HudsonBuildStatus buildStatus = new HudsonBuildStatus(jenkinsBuildStatusForIssue10, buildDefinitionSetting);
-            buildStatus.Name = "Name";
-            buildStatus.BuildDefinitionId = "BuildDefinitionId";
+            
             Assert.AreEqual(BuildStatusEnum.Working, buildStatus.BuildStatusEnum);
-
             Assert.AreEqual("BuildDefinitionId", buildStatus.BuildDefinitionId);
             Assert.AreEqual("Name", buildStatus.Name);
             Assert.AreEqual("anonymous", buildStatus.RequestedBy);
@@ -27,14 +27,23 @@ namespace SirenOfShame.Test.Unit.CiEntryPointBuildStatus
             Assert.IsNull(buildStatus.Comment);
             Assert.AreEqual(new DateTime(2011, 12, 23, 21, 8, 22, 465), buildStatus.FinishedTime); // timestamp+duration
         }
-        
+
         //[TestMethod]
         //public void HudsonBuildStatus_Issues7()
         //{
         //    var jenkinsBuildStatusForIssue10 = ResourceManager.JenkinsBuildStatusForIssue10;
         //    BuildDefinitionSetting buildDefinitionSetting = new BuildDefinitionSetting();
+        //    buildDefinitionSetting.Name = "Name";
+        //    buildDefinitionSetting.Id = "BuildDefinitionId";
         //    HudsonBuildStatus buildStatus = new HudsonBuildStatus(jenkinsBuildStatusForIssue10, buildDefinitionSetting);
+
         //    Assert.AreEqual(BuildStatusEnum.Working, buildStatus.BuildStatusEnum);
+        //    Assert.AreEqual("BuildDefinitionId", buildStatus.BuildDefinitionId);
+        //    Assert.AreEqual("Name", buildStatus.Name);
+        //    Assert.AreEqual("anonymous", buildStatus.RequestedBy);
+        //    Assert.AreEqual(new DateTime(2011, 12, 23, 21, 8, 21), buildStatus.StartedTime);
+        //    Assert.IsNull(buildStatus.Comment);
+        //    Assert.AreEqual(new DateTime(2011, 12, 23, 21, 8, 22, 465), buildStatus.FinishedTime); // timestamp+duration
         //}
     }
 }
