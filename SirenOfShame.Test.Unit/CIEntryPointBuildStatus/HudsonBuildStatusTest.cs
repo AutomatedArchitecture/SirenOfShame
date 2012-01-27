d71808c13a31f664f77b09c7dcb899018dc182ab
lprichar <none@none>
2012-01-26 22:37:08 -0500

    Unit tests for existing Jenkins build status parsing

---
+++ SirenOfShame.Test.Unit/CIEntryPointBuildStatus/HudsonBuildStatusTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using HudsonServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SirenOfShame.Lib.Settings;
+using SirenOfShame.Lib.Watcher;
+using SirenOfShame.Test.Unit.Resources;
+
+namespace SirenOfShame.Test.Unit.CiEntryPointBuildStatus
+{
+    [TestClass]
+    public class HudsonBuildStatusTest
+    {
+        [TestMethod]
+        public void HudsonBuildStatus_PassingBuildNoComment()
+        {
+            var jenkinsBuildStatusForIssue10 = ResourceManager.JenkinsPassingBuild;
+            BuildDefinitionSetting buildDefinitionSetting = new BuildDefinitionSetting();
+            HudsonBuildStatus buildStatus = new HudsonBuildStatus(jenkinsBuildStatusForIssue10, buildDefinitionSetting);
+            buildStatus.Name = "Name";
+            buildStatus.BuildDefinitionId = "BuildDefinitionId";
+            Assert.AreEqual(BuildStatusEnum.Working, buildStatus.BuildStatusEnum);
+
+            Assert.AreEqual("BuildDefinitionId", buildStatus.BuildDefinitionId);
+            Assert.AreEqual("Name", buildStatus.Name);
+            Assert.AreEqual("anonymous", buildStatus.RequestedBy);
+            Assert.AreEqual(new DateTime(2011, 12, 23, 21, 8, 21), buildStatus.StartedTime);
+            Assert.IsNull(buildStatus.Comment);
+            Assert.AreEqual(new DateTime(2011, 12, 23, 21, 8, 22, 465), buildStatus.FinishedTime); // timestamp+duration
+        }
+        
+        //[TestMethod]
+        //public void HudsonBuildStatus_Issues7()
+        //{
+        //    var jenkinsBuildStatusForIssue10 = ResourceManager.JenkinsBuildStatusForIssue10;
+        //    BuildDefinitionSetting buildDefinitionSetting = new BuildDefinitionSetting();
+        //    HudsonBuildStatus buildStatus = new HudsonBuildStatus(jenkinsBuildStatusForIssue10, buildDefinitionSetting);
+        //    Assert.AreEqual(BuildStatusEnum.Working, buildStatus.BuildStatusEnum);
+        //}
+    }
+}
---
+++ SirenOfShame.Test.Unit/Resources/ResourceManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Xml.Linq;
+
+namespace SirenOfShame.Test.Unit.Resources
+{
+    public static class ResourceManager
+    {
+        private static string GetResource(string resourceName)
+        {
+            string fullResourceName = "SirenOfShame.Test.Unit.Resources." + resourceName;
+
+            using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(fullResourceName))
+            {
+                try
+                {
+                    if (stream == null) throw new Exception("Stream was null");
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        return reader.ReadToEnd();
+                    }
+                }
+                catch (Exception e)
+                {
+                    throw new Exception("Error retrieving Resource: '" + resourceName + "'. " + e);
+                }
+
+            }
+        }
+
+        public static XDocument JenkinsBuildStatusForIssue10
+        {
+            get { return XDocument.Parse(GetResource("JenkinsBuildStatusForIssue10.xml")); }
+        }
+
+        public static XDocument JenkinsPassingBuild
+        {
+            get { return XDocument.Parse(GetResource("JenkinsPassingBuild.xml")); }
+        }
+    }
+}
