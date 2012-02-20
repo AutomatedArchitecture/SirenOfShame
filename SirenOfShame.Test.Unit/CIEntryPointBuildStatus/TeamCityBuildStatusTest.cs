1c14796a4084990ca80f4598ebfb0f6c07907395
lprichar <none@none>
2012-02-19 23:19:22 -0500

    Now retrieving Url and BuildId for TeamCity

---
+++ SirenOfShame.Test.Unit/CIEntryPointBuildStatus/TeamCityBuildStatusTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SirenOfShame.Lib.Settings;
+using SirenOfShame.Lib.Watcher;
+using SirenOfShame.Test.Unit.CiEntryPointBuildStatus;
+using SirenOfShame.Test.Unit.Resources;
+using TeamCityServices;
+
+namespace SirenOfShame.Test.Unit.CIEntryPointBuildStatus
+{
+    [TestClass]
+    public class TeamCityBuildStatusTest
+    {
+        [TestMethod]
+        public void HudsonBuildStatus_PassingBuildNoComment()
+        {
+            var teamCityFailingBuild = ResourceManager.TeamCityFailingBuild;
+            var teamCityFailingChange = ResourceManager.TeamCityFailingChange;
+            BuildDefinitionSetting buildDefinitionSetting = new BuildDefinitionSetting();
+            buildDefinitionSetting.Name = "Name";
+            buildDefinitionSetting.Id = "BuildDefinitionId";
+            var buildStatus = new TeamCityBuildStatus(buildDefinitionSetting, teamCityFailingBuild, teamCityFailingChange);
+
+            Assert.AreEqual(BuildStatusEnum.Broken, buildStatus.BuildStatusEnum);
+            Assert.AreEqual("BuildDefinitionId", buildStatus.BuildDefinitionId);
+            Assert.AreEqual("BuildConfig4", buildStatus.Name);
+            Assert.AreEqual("lee", buildStatus.RequestedBy);
+            Assert.AreEqual(new DateTime(2012, 1, 27, 0, 15, 45, 0), buildStatus.StartedTime, HudsonBuildStatusTest.DateAsCode(buildStatus.StartedTime.Value));
+            string expectedComment = @"Merge branch 'master' of C:\dev\CiTest
+
+Conflicts:
+	CiTest/Program.cs".Replace("\r", "");
+            Assert.AreEqual(expectedComment.Replace("\r\n", "\r"), buildStatus.Comment);
+            Assert.AreEqual(new DateTime(2012, 1, 27, 0, 16, 2, 0), buildStatus.FinishedTime, HudsonBuildStatusTest.DateAsCode(buildStatus.FinishedTime.Value)); // timestamp+duration
+            Assert.AreEqual("http://win7ci:8080/viewLog.html?buildId=35&buildTypeId=bt2", buildStatus.Url);
+            Assert.AreEqual(35, buildStatus.BuildId);
+        }
+    }
+}
--- TeamCityServices/TeamCityBuildStatus.cs
+++ TeamCityServices/TeamCityBuildStatus.cs
@@ -84,6 +84,22 @@ namespace TeamCityServices
                     FinishedTime = GetTeamCityDate(finishedTimeStr);
                     BuildStatusEnum = ToBuildStatusEnum(status);
                 }
+
+                Url = buildResultXDoc.Root.AttributeValueOrDefault("webUrl");
+                if (Url != null) Url = Url.Trim();
+
+                var buildId = buildResultXDoc.Root.AttributeValueOrDefault("id");
+                try
+                {
+                    if (!string.IsNullOrWhiteSpace(buildId))
+                    {
+                        BuildId = int.Parse(buildId);
+                    }
+                } catch (Exception)
+                {
+                    _log.Error("Error parsing BuildId: " + buildId);
+                    throw;
+                }
             } 
             catch (Exception)
             {
