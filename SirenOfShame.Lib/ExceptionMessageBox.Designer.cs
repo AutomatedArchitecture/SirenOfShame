7986f2eb1e51b13e0305146a543f3ad0a03b23bd
lprichar <none@none>
2012-04-28 13:20:37 -0400

    Added optional conect info to error reporting dialog

--- SirenOfShame.Lib/ExceptionMessageBox.Designer.cs
+++ SirenOfShame.Lib/ExceptionMessageBox.Designer.cs
@@ -34,20 +34,22 @@
             this._showMore = new System.Windows.Forms.Button();
             this._exception = new System.Windows.Forms.TextBox();
             this._cancel = new System.Windows.Forms.Button();
+            this._contact = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // _message
             // 
             this._message.Location = new System.Drawing.Point(12, 9);
             this._message.Name = "_message";
-            this._message.Size = new System.Drawing.Size(376, 41);
+            this._message.Size = new System.Drawing.Size(346, 18);
             this._message.TabIndex = 0;
             this._message.Text = "Error Message";
             // 
             // _ok
             // 
             this._ok.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this._ok.Location = new System.Drawing.Point(313, 60);
+            this._ok.Location = new System.Drawing.Point(283, 66);
             this._ok.Name = "_ok";
             this._ok.Size = new System.Drawing.Size(75, 23);
             this._ok.TabIndex = 1;
@@ -58,7 +60,7 @@
             // _showMore
             // 
             this._showMore.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this._showMore.Location = new System.Drawing.Point(151, 60);
+            this._showMore.Location = new System.Drawing.Point(121, 66);
             this._showMore.Name = "_showMore";
             this._showMore.Size = new System.Drawing.Size(75, 23);
             this._showMore.TabIndex = 2;
@@ -68,15 +70,15 @@
             // 
             // _exception
             // 
-            this._exception.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
+            this._exception.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this._exception.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this._exception.Location = new System.Drawing.Point(12, 53);
+            this._exception.Location = new System.Drawing.Point(15, 56);
             this._exception.Multiline = true;
             this._exception.Name = "_exception";
             this._exception.ReadOnly = true;
-            this._exception.Size = new System.Drawing.Size(376, 0);
+            this._exception.Size = new System.Drawing.Size(343, 4);
             this._exception.TabIndex = 3;
             this._exception.WordWrap = false;
             // 
@@ -84,7 +86,7 @@
             // 
             this._cancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this._cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this._cancel.Location = new System.Drawing.Point(232, 60);
+            this._cancel.Location = new System.Drawing.Point(202, 66);
             this._cancel.Name = "_cancel";
             this._cancel.Size = new System.Drawing.Size(75, 23);
             this._cancel.TabIndex = 4;
@@ -92,13 +94,33 @@
             this._cancel.UseVisualStyleBackColor = true;
             this._cancel.Click += new System.EventHandler(this.CancelClick);
             // 
+            // _contact
+            // 
+            this._contact.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this._contact.Location = new System.Drawing.Point(151, 30);
+            this._contact.Name = "_contact";
+            this._contact.Size = new System.Drawing.Size(207, 20);
+            this._contact.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "E-mail or twitter (optional)";
+            // 
             // ExceptionMessageBox
             // 
             this.AcceptButton = this._ok;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this._cancel;
-            this.ClientSize = new System.Drawing.Size(400, 95);
+            this.ClientSize = new System.Drawing.Size(370, 101);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this._contact);
             this.Controls.Add(this._cancel);
             this.Controls.Add(this._exception);
             this.Controls.Add(this._showMore);
@@ -121,5 +143,7 @@
         private System.Windows.Forms.Button _showMore;
         private System.Windows.Forms.TextBox _exception;
         private System.Windows.Forms.Button _cancel;
+        private System.Windows.Forms.TextBox _contact;
+        private System.Windows.Forms.Label label1;
     }
 }
