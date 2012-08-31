edcbfe60924598220f04e883d41d0d510ecab368
lprichar <none@none>
2012-08-31 00:56:18 -0400

    Major UI cleanup continued
    --HG--
    branch : MetroUi

---
+++ SirenOfShame.Lib/SosButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace SirenOfShame.Lib
+{
+    public sealed class SosButton : Button
+    {
+        public SosButton()
+        {
+            ForeColor = Color.White;
+            FlatStyle = FlatStyle.Flat;
+            FlatAppearance.BorderColor = Color.FromArgb(40, 38, 39);
+            FlatAppearance.BorderSize = 0;
+            FlatAppearance.MouseDownBackColor = Color.FromArgb(186, 55, 0);
+            FlatAppearance.MouseOverBackColor = Color.FromArgb(196, 65, 0);
+            BackColor = Color.FromArgb(68, 68, 68);
+        }
+    }
+}
