d4e93c258a520a9f1d70d898e791a87b0ae36ded
Noam Lewis <lenoam+BDB@gmail.com>
2013-07-25 11:28:42 -0400

    Add new CLI project for controlling the siren from the command line (useful for automating sirens from external applications, such as email filters)

---
+++ SirenOfShame.CLI/DeviceController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SirenOfShame.Lib.Device;
+
+namespace SirenOfShame.CLI
+{
+    public class DeviceController
+    {
+        ISirenOfShameDevice _sirenOfShameDevice;
+
+        public DeviceController() {
+            this._sirenOfShameDevice = new SirenOfShameDevice();
+            this._sirenOfShameDevice.Connected += sirenOfShameDevice_Connected;
+            this._sirenOfShameDevice.Disconnected += sirenOfShameDevice_Disconnected;
+
+        }
+
+        public void PlayAlarm(TimeSpan timeSpan, int? ledPatternIndex, int? audioPatternIndex)
+        {
+            var ledPatterns = this._sirenOfShameDevice.LedPatterns.ToArray();
+            if (ledPatternIndex.HasValue)
+            {
+                this._sirenOfShameDevice.PlayLightPattern(ledPatterns[ledPatternIndex.Value % ledPatterns.Length], timeSpan);
+            }
+            var audioPattern = this._sirenOfShameDevice.AudioPatterns.ToArray();
+            if (audioPatternIndex.HasValue)
+            {
+                this._sirenOfShameDevice.PlayAudioPattern(audioPattern[audioPatternIndex.Value % audioPattern.Length], timeSpan);
+            }
+        }
+
+        public bool TryConnect()
+        {
+            return this._sirenOfShameDevice.TryConnect();
+        }
+
+        public static void Log(string message)
+        {
+            Console.Error.WriteLine(message);
+        }
+
+
+        protected void sirenOfShameDevice_Disconnected(object sender, EventArgs e)
+        {
+            Log("Device disconnected.");
+        }
+
+        protected void sirenOfShameDevice_Connected(object sender, EventArgs e)
+        {
+            Log("Device connected.");
+            var deviceInfo = _sirenOfShameDevice.ReadDeviceInfo();
+            Log(deviceInfo.ToString());
+
+
+        }
+    }
+}
---
+++ SirenOfShame.CLI/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using SirenOfShame.Lib.Device;
+
+namespace SirenOfShame.CLI
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            Console.Error.WriteLine("Usage: <program.exe> [led pattern index] [audio pattern index]");
+            Console.Error.Write("Connecting device...");
+            var deviceController = new DeviceController();
+            while (false == deviceController.TryConnect())
+            {
+                Console.Error.Write(".");
+                Thread.Sleep(new TimeSpan(0, 0, 1));
+            }
+            Console.Error.WriteLine();
+
+            int? lightIndex = TryGetIntArg(args, 0);
+            int? audioIndex = TryGetIntArg(args, 1);
+            deviceController.PlayAlarm(new TimeSpan(0, 0, 10), lightIndex, audioIndex);
+            return 0;
+        }
+
+        private static int? TryGetIntArg(string[] args, int index)
+        {
+            var lightIndexStr = TryGetArrayItem(args, index, null);
+            int? lightIndex = null;
+            if (false == String.IsNullOrWhiteSpace(lightIndexStr))
+            {
+                lightIndex = Int32.Parse(lightIndexStr);
+            }
+            return lightIndex;
+        }
+
+        private static string TryGetArrayItem(string[] args, int index, string defaultValue)
+        {
+            if (args.Length > index) {
+                return args[index];
+            }
+            return defaultValue;
+        }
+    }
+}
