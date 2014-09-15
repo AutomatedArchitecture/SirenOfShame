42cbc62afb896cd4b3107ffa1330220e7688bdfb
Lee Richardson <github@leerichardson.com>
2014-09-14 21:24:51 -0400

    Upgraded to 2.3.4

--- SirenOfShame.WixSetup/sign.bat
+++ SirenOfShame.WixSetup/sign.bat
@@ -1,2 +1,12 @@
---usage sign.bat [pfxcert] [password] [filename]
-"c:\Program Files (x86)\Windows Kits\8.1\bin\x64\signtool.exe" sign /f %1 /p %2 /d %3 %3
+@echo off
+:: usage siren.bat [filename]
+
+if "%1" == "" (
+  echo usage: siren.bat [filename]
+  goto :end
+)
+
+rename SirenOfShame.WixSetup.msi %1
+"c:\Program Files (x86)\Windows Kits\8.1\bin\x64\signtool.exe" sign /f "C:\Temp\Cert.pfx" /p password /d %1 %1
+
+:end
