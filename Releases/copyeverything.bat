8bb79cf846bbff8c9f00b8d118950a451fd02778
Lee Richardson <github@leerichardson.com>
2014-10-31 00:52:19 -0400

    Fixed more deployment issues with 2.3.7

--- Releases/copyeverything.bat
+++ Releases/copyeverything.bat
@@ -5,6 +5,7 @@ REM this next line is stupid, but it's required to upgrade from 1.6 because back
 mkdir BuildArtifacts\%1\Plugins
 copy ..\SirenOfShame\bin\SirenOfShame.Lib.dll .\BuildArtifacts\%1\Plugins\SirenOfShame.Lib.dll
 copy ..\SirenOfShame\bin\SoxLib.dll .\BuildArtifacts\%1\SoxLib.dll
+
 copy ..\SirenOfShame\bin\Plugins\BuildBotServices.dll .\BuildArtifacts\%1\Plugins\BuildBotServices.dll
 copy ..\SirenOfShame\bin\Plugins\TravisCiServices.dll .\BuildArtifacts\%1\Plugins\TravisCiServices.dll
 copy ..\SirenOfShame\bin\Plugins\BambooServices.dll .\BuildArtifacts\%1\Plugins\BambooServices.dll
@@ -13,6 +14,7 @@ copy ..\SirenOfShame\bin\Plugins\HudsonServices.dll .\BuildArtifacts\%1\Plugins\
 copy ..\SirenOfShame\bin\Plugins\TeamCityServices.dll .\BuildArtifacts\%1\Plugins\TeamCityServices.dll
 copy ..\SirenOfShame\bin\Plugins\TfsServices.dll .\BuildArtifacts\%1\Plugins\TfsServices.dll
 copy ..\SirenOfShame\bin\Plugins\UsbLib.dll .\BuildArtifacts\%1\Plugins\UsbLib.dll
+copy ..\SirenOfShame\bin\SoxLib.dll .\BuildArtifacts\%1\Plugins\SoxLib.dll
 copy ..\Libs\ZedGraph.dll .\BuildArtifacts\%1\ZedGraph.dll
 copy ..\Libs\TeamFoundation\* .\BuildArtifacts\%1\
 copy ..\Libs\sox-14.3.2\zlib1.dll .\BuildArtifacts\1%\Sox\zlib1.dll
