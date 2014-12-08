65e1199ce9a783e309dbaa543b8ed306264085ac
Lee Richardson <github@leerichardson.com>
2014-12-07 21:10:50 -0500

    Deleting orphan TFS code

--- TfsServices/Configuration/MyTfsBuildDefinition.cs
+++ TfsServices/Configuration/MyTfsBuildDefinition.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using System.Windows.Forms;
-using log4net;
 using Microsoft.TeamFoundation.Build.Client;
-using Microsoft.TeamFoundation.VersionControl.Client;
-using SirenOfShame.Lib;
 using SirenOfShame.Lib.Settings;
 
 namespace TfsServices.Configuration
@@ -14,15 +8,12 @@ namespace TfsServices.Configuration
     public class MyTfsBuildDefinition : MyBuildDefinition
     {
         private readonly IBuildDefinition _buildDefinition;
-        private readonly MyTfsProject _myTfsProject;
-        private static readonly ILog Log = MyLogManager.GetLogger(typeof(MyTfsBuildDefinition));
         private readonly MyBuildServer _myBuildServer;
 
         public MyTfsBuildDefinition(IBuildDefinition buildDefinition, MyTfsProject myTfsProject)
         {
             _buildDefinition = buildDefinition;
-            _myTfsProject = myTfsProject;
-            _myBuildServer = new MyBuildServer(_buildDefinition.BuildServer, _myTfsProject);
+            _myBuildServer = new MyBuildServer(_buildDefinition.BuildServer, myTfsProject);
         }
 
         public override string Name
@@ -48,77 +39,6 @@ namespace TfsServices.Configuration
         public TreeNode GetAsNode(bool active)
         {
             return new TreeNode(Name) { Tag = Id, Checked = active };
-        }
-
-        private const int DELETION_ID = 0;
-
-        public MyChangeset GetLatestChangeset()
-        {
-            try
-            {
-                // generally there is only one workspace mapping per build definition, but there could be >1
-                IList<string> workspaceMappingServerUrls = GetNonCloakedWorkspaceMappingServerUrls().ToList();
-                var noWorkspaceMappings = !workspaceMappingServerUrls.Any();
-                if (noWorkspaceMappings)
-                {
-                    Log.Warn(string.Format("Build definition {0} does not have any workspace mappings so can't retrieve comments", Id));
-                    return null;
-                }
-
-                Changeset maxChangeset = null;
-                var versionControlServer = _myTfsProject.GetVersionControlServer();
-                foreach (var workspaceMappingServerUrl in workspaceMappingServerUrls)
-                {
-                    IEnumerable changesets = GetChangesetsFromServer(versionControlServer, workspaceMappingServerUrl);
-                    Changeset changeset = GetMostRecentChangeset(changesets);
-                    var thisIsTheFirstWorkspaceMapping = maxChangeset == null;
-                    var theCurrentChangesetIsMoreRecentThanTheMax = thisIsTheFirstWorkspaceMapping || changeset.ChangesetId > maxChangeset.ChangesetId;
-                    if (theCurrentChangesetIsMoreRecentThanTheMax)
-                    {
-                        maxChangeset = changeset;
-                    }
-                }
-
-                return maxChangeset == null ? null : new MyChangeset(maxChangeset);
-            }
-            catch (Exception ex)
-            {
-                Log.Error("Unable to retrieve comments for build definition " + Id, ex);
-                return null; // errors in getting comments 
-            }
-        }
-
-        private static Changeset GetMostRecentChangeset(IEnumerable changesets)
-        {
-            return changesets == null ? null : changesets.Cast<Changeset>().FirstOrDefault();
-        }
-
-        private static IEnumerable GetChangesetsFromServer(VersionControlServer versionControlServer, string workspaceMappingServerUrl)
-        {
-            return versionControlServer.QueryHistory(workspaceMappingServerUrl,
-                                                     VersionSpec.Latest,
-                                                     DELETION_ID,
-                                                     RecursionType.Full,
-                                                     null,
-                                                     null,
-                                                     VersionSpec.Latest,
-                                                     1,
-                                                     true,
-                                                     false,
-                                                     true);
-        }
-
-        // Exclude cloaked mappings - they can't have changesets.
-        private IEnumerable<string> GetNonCloakedWorkspaceMappingServerUrls()
-        {
-            return _buildDefinition.Workspace.Mappings
-                .Where(m => m.MappingType != WorkspaceMappingType.Cloak)
-                .Select(m => m.ServerItem);
-        }
-
-        public string ConvertTfsUriToUrl(Uri uri)
-        {
-            return _myTfsProject.ConvertTfsUriToUrl(uri);
         }
     }
 }
