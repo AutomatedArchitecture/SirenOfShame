87993e4fdaa52b060a15d30c7db8408c5f692bc6
Lee Richardson <git@leerichardson.com>
2015-03-01 14:52:03 -0500

    Added a PCL and a unit test project in preparation for dealing with encryption in a cross platform way

---
+++ SirenOfShame.Extruder.Mac.Test.Unit/Services/TripleDesStringEncryptorTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using SirenOfShame.Extruder.PclLib;
+
+namespace SirenOfShame.Extruder.Mac.Test.Unit
+{
+	[TestFixture()]
+	public class TripleDesStringEncryptorTest
+	{
+		[Test ()]
+		public void Encrypt_GivenNull_ReturnsNull ()
+		{
+			var tdse = new TripleDesStringEncryptor ();
+			Assert.IsNull(tdse.EncryptString (null));
+		}
+
+		[Test ()]
+		public void Decrypt_GivenNull_ReturnsNull ()
+		{
+			var tdse = new TripleDesStringEncryptor ();
+			Assert.IsNull(tdse.DecryptString (null));
+		}
+	}
+}
+
