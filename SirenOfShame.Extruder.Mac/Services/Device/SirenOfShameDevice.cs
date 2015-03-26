38e21f2c673c71e4219bbba9dc721c43a9711fe6
Lee Richardson <git@leerichardson.com>
2015-03-25 21:27:50 -0400

    Simple logging

--- SirenOfShame.Extruder.Mac/Services/Device/SirenOfShameDevice.cs
+++ SirenOfShame.Extruder.Mac/Services/Device/SirenOfShameDevice.cs
@@ -7,15 +7,24 @@ namespace SirenOfShame.Extruder.Mac
 	public class SirenOfShameDevice : IDisposable
 	{
 		private UsbDevice _myUsbDevice;
+		ILog _log = MyLogManager.GetLogger (typeof(SirenOfShameDevice));
 
 		public SirenOfShameDevice ()
 		{
 		}
 
+		const int productId = 0x0646;
+		const int vendorId = 0x16D0;
+
 		public bool TryConnect() {
-			UsbDeviceFinder MyUsbFinder = new UsbDeviceFinder(0x16D0, 0x0646);
+			UsbDeviceFinder MyUsbFinder = new UsbDeviceFinder (vendorId, productId);
 			_myUsbDevice = UsbDevice.OpenUsbDevice (MyUsbFinder);
-			return _myUsbDevice == null;
+			if (_myUsbDevice == null)
+				return false;
+
+			_log.Debug ("Device connected");
+
+			return true;
 		}
 
 		#region IDisposable implementation
---
+++ SirenOfShame.Extruder.Mac/Services/MyLogManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SirenOfShame.Extruder.Mac
+{
+	public interface ILog {
+		void Debug (string deviceNotFound);
+
+		void Info (string startingFirmwareUpgrade);
+
+		void Error (string readFailed, Exception ex);
+	}
+
+	public class ConsoleLog : ILog {
+		public void Debug (string log)
+		{
+			Console.WriteLine (log);
+		}
+		public void Info (string log)
+		{
+			Console.WriteLine (log);
+		}
+		public void Error (string log, Exception ex)
+		{
+			Console.WriteLine (log + "\n" + ex);
+		}
+	}
+
+	public class MyLogManager
+	{
+		public static ILog GetLogger (Type type)
+		{
+			return new ConsoleLog ();
+		}
+
+		public MyLogManager ()
+		{
+		}
+	}
+}
+
