8eb43216b9328e7d06d8b3017b07f7b0adb819e9
Lee Richardson <git@leerichardson.com>
2015-03-25 20:34:19 -0400

    Refactored into a SirenOfShameDevice class

---
+++ SirenOfShame.Extruder.Mac/Services/Device/SirenOfShameDevice.cs
@@ -0,0 +1,33 @@
+﻿using LibUsbDotNet.Main;
+using LibUsbDotNet;
+using System;
+
+namespace SirenOfShame.Extruder.Mac
+{
+	public class SirenOfShameDevice : IDisposable
+	{
+		private UsbDevice _myUsbDevice;
+
+		public SirenOfShameDevice ()
+		{
+		}
+
+		public bool TryConnect() {
+			UsbDeviceFinder MyUsbFinder = new UsbDeviceFinder(0x16D0, 0x0646);
+			_myUsbDevice = UsbDevice.OpenUsbDevice (MyUsbFinder);
+			return _myUsbDevice == null;
+		}
+
+		#region IDisposable implementation
+
+		public void Dispose ()
+		{
+			// Free usb resources.
+			// This is necessary for libusb-1.0 and Linux compatibility.
+			UsbDevice.Exit();
+		}
+
+		#endregion
+	}
+}
+
