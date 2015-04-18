fc63bee690854a706a568e26240d95786737304b
Lee Richardson <git@leerichardson.com>
2015-04-18 15:58:25 -0400

    Whoaaaaaaa!! Lighting lights on OSX via hidapi!!!!!

--- MyCppLibrary/MyCppLib/SosDevice.cpp
+++ MyCppLibrary/MyCppLib/SosDevice.cpp
@@ -9,14 +9,17 @@
 #include <string.h>
 #include <iostream>
 #include <sstream>
-#include <libusb.h>
+#include "hidapi.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include "SosDevice.h"
 #include "usbPackets.h"
 
 uint16_t sosVendorId = 5840;
 uint16_t sosProductId = 1606;
 int sosPacketSize = 1 + 37; // report id + packet length
+
+#define MAX_STR 255
 
 // From the HID spec:
 static const int HID_REPORT_GET = 0x01;
@@ -27,31 +30,9 @@ static const int HID_REPORT_TYPE_FEATURE = 0x03;
 
 static const int INTERFACE_NUMBER = 0;
 
-static const int CONTROL_REQUEST_TYPE_IN = LIBUSB_ENDPOINT_IN | LIBUSB_REQUEST_TYPE_CLASS | LIBUSB_RECIPIENT_INTERFACE;
-static const int CONTROL_REQUEST_TYPE_OUT = LIBUSB_ENDPOINT_OUT | LIBUSB_REQUEST_TYPE_CLASS | LIBUSB_RECIPIENT_INTERFACE;
-
-static int perr(char const *format, ...)
-{
-	va_list args;
-	int r;
-    
-	va_start (args, format);
-	r = vfprintf(stderr, format, args);
-	va_end(args);
-    
-	return r;
-}
-
-static struct libusb_device *findSos() {
-	libusb_device_handle *handle;
-	libusb_device *dev;
-	handle = libusb_open_device_with_vid_pid(NULL, sosVendorId, sosProductId);
-	dev = libusb_get_device(handle);
-    return dev;
-}
-
 void initControlPacket(UsbControlPacket *packet) {
     memset(packet, 0, sizeof(UsbControlPacket));
+    packet->reportId = 1;
     packet->controlByte1 = 0;
     packet->audioMode = 0xff;
     packet->ledMode = 0xff;
@@ -66,49 +47,23 @@ void initControlPacket(UsbControlPacket *packet) {
     packet->manualLeds4 = 0xff;
 }
 
-std::string setOutputReport(int reportId, unsigned char *buf, int bufSize, libusb_device_handle *devHandle) {
+std::string setOutputReport(int reportId, unsigned char *buf, int bufSize, hid_device *devHandle) {
     std::ostringstream strstrm;
     
-    int claimResult = libusb_claim_interface(devHandle, INTERFACE_NUMBER);
-    if(claimResult != 0) {
-        strstrm << "usb_claim_interface: " << " " << claimResult << " " << libusb_strerror((enum libusb_error)claimResult) << std::endl;
-        return strstrm.str();
-    }
+    int result = hid_write(devHandle, buf, bufSize);
+    std::cout << "hid_write: " << result;
     
-//    int LIBUSB_CALL libusb_control_transfer(libusb_device_handle *dev_handle,
-//                                            uint8_t request_type, uint8_t bRequest, uint16_t wValue, uint16_t wIndex,
-//                                            unsigned char *data, uint16_t wLength, unsigned int timeout);
-
-    
-    int bytesSent = libusb_control_transfer(
-                                    devHandle,
-                                    CONTROL_REQUEST_TYPE_OUT,
-                                    HID_REPORT_SET,
-                                    (HID_REPORT_TYPE_INPUT << 8) | reportId,
-                                    INTERFACE_NUMBER,
-                                    buf,
-                                    bufSize,
-                                    10000);
-    if(bytesSent < 0) {
-        strstrm << "usb_control_msg: " << " " << bytesSent; // << " " << libusb_strerror();
-        return strstrm.str();
-    }
-    
-    int releaseResult = libusb_release_interface(devHandle, INTERFACE_NUMBER);
-    if(releaseResult != 0) {
-        strstrm << "usb_release_interface: " << " " << releaseResult << " " << libusb_strerror((enum libusb_error)releaseResult);
-        return strstrm.str();
-    }
     return std::string("Success!");
 }
 
-std::string sendControlPacket(libusb_device_handle *devHandle) {
+std::string sendControlPacket(hid_device *devHandle) {
     UsbControlPacket usbControlPacket;
     initControlPacket(&usbControlPacket);
     
-    usbControlPacket.manualLeds0 = 200;
-    usbControlPacket.manualLeds2 = 200;
-    usbControlPacket.manualLeds4 = 200;
+    usbControlPacket.ledMode = LED_MODE_INTERNAL_START;
+    usbControlPacket.ledPlayDuration = 500;
+    usbControlPacket.audioMode = 0;
+    usbControlPacket.audioPlayDuration = 500;
     try {
         return setOutputReport(USB_REPORTID_OUT_CONTROL, (unsigned char*)&usbControlPacket, sizeof(usbControlPacket), devHandle);
     } catch(std::exception &ex) {
@@ -117,29 +72,38 @@ std::string sendControlPacket(libusb_device_handle *devHandle) {
 }
 
 const std::string lightLights() {
-    int r;
     std::ostringstream strstrm;
-//    libusb_device *dev = findSos();
-//    if (dev == NULL) {
-//        return std::string("No device found");
-//    }
-	r = libusb_init(NULL);
-    libusb_device_handle *devHandle = libusb_open_device_with_vid_pid(NULL, sosVendorId, sosProductId);
-    if(devHandle == NULL) {
-		perr("  Failed.\n");
-        strstrm << "dev handle was null: "; // << " " << libusb_strerror();
-        return strstrm.str();
-    }
+    int res;
+	hid_device *handle;
+    wchar_t wstr[MAX_STR];
     
-    int detachResult = libusb_detach_kernel_driver(devHandle, INTERFACE_NUMBER);
-    if(detachResult != 0 && detachResult != -61) {
-        strstrm << "usb_detach_kernel_driver_np: " << " " << detachResult << " " << libusb_strerror((enum libusb_error)detachResult);
-        return strstrm.str();
-    }
+    res = hid_init();
     
-    return sendControlPacket(devHandle);
+    // Open the device using the VID, PID,
+	// and optionally the Serial number.
+	handle = hid_open(sosVendorId, sosProductId, NULL);
 
- 	libusb_exit(NULL);
+    res = hid_get_manufacturer_string(handle, wstr, MAX_STR);
+    wprintf(L"Manufacturer String: %s\n", wstr);
+    
+    // Read the Product String
+	res = hid_get_product_string(handle, wstr, MAX_STR);
+	wprintf(L"Product String: %s\n", wstr);
+    
+    // Read the Serial Number String
+	res = hid_get_serial_number_string(handle, wstr, MAX_STR);
+	wprintf(L"Serial Number String: (%d) %s\n", wstr[0], wstr);
+    
+    // Read Indexed String 1
+	res = hid_get_indexed_string(handle, 1, wstr, MAX_STR);
+	wprintf(L"Indexed String 1: %s\n", wstr);
+    
+    sendControlPacket(handle);
+    
+    // Finalize the hidapi library
+	res = hid_exit();
+    
+	return std::string("Success");
 }
 
 extern "C" std::string *GetHelloCount(void) {
