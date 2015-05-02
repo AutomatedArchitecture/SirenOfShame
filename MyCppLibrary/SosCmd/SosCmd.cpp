438e3fd8b11f9925779b9e03c44e5a80c0ba16a5
Lee Richardson <git@leerichardson.com>
2015-05-02 15:41:41 -0400

    Get command line app using the library

--- MyCppLibrary/SosCmd/SosCmd.cpp
+++ MyCppLibrary/SosCmd/SosCmd.cpp
@@ -1,11 +1,14 @@
 // SosCmd.cpp : Defines the entry point for the console application.
 //
 
+#include <iostream>
 #include "stdafx.h"
-
+#include "SosDevice.h"
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	GetHelloCount();
+	std::cin.get();
 	return 0;
 }
 
