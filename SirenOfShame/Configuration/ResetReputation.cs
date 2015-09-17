d5b6f725ee302cdc68bd02e5d9bd3976955d1d9b
Lee Richardson <github@leerichardson.com>
2015-09-16 22:03:11 -0400

    Ability to reset only

--- SirenOfShame/Configuration/ResetReputation.cs
+++ SirenOfShame/Configuration/ResetReputation.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows.Forms;
 using SirenOfShame.Lib.Settings;
+using SirenOfShame.Lib.Watcher;
 
 namespace SirenOfShame.Configuration
 {
     public partial class ResetReputation : Form
     {
         private readonly SirenOfShameSettings _settings;
+        public SosDb SosDb = new SosDb();
 
         public ResetReputation(SirenOfShameSettings settings)
         {
@@ -52,6 +55,33 @@ namespace SirenOfShame.Configuration
         private void ResetButton_Click(object sender, EventArgs e)
         {
             _settings.Backup();
+            foreach (var person in _settings.People)
+            {
+                person.TotalBuilds = 0;
+                person.FailedBuilds = 0;
+                person.Achievements = new List<AchievementSetting>();
+                person.CumulativeBuildTime = 0;
+                person.CurrentBuildRatio = 0;
+                person.CurrentSuccessInARow = 0;
+                person.LowestBuildRatioAfter50Builds = 0;
+                person.MaxBuildsInOneDay = 0;
+                person.NumberOfTimesFixedSomeoneElsesBuild = 0;
+                person.NumberOfTimesPerformedBackToBackBuilds = 0;
+            }
+            _settings.Save();
+
+            if (ResetAndRebuildFromStart.Checked || ResetAndRebuildSinceDate.Checked)
+            {
+                SosDb.GetMostRecentNewsItems(_settings, OnGetNewsItems);
+            }
+        }
+
+        private void OnGetNewsItems(IList<NewNewsItemEventArgs> newNewsItemEventArgses)
+        {
+            foreach (var newNewsItemEventArgse in newNewsItemEventArgses)
+            {
+                
+            }
         }
     }
 }
