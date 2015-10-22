3bfa170bce77f7fee906fb82d0cbda215434dbf6
Lee Richardson <github@leerichardson.com>
2015-10-21 20:47:27 -0400

    Fixing the build, adding files necessary to compile

---
+++ SirenOfShame.Lib/Services/GravatarService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SirenOfShame.Lib.Services
+{
+    public class GravatarService
+    {
+        private const int DEFAULT_IMAGE_SIZE = 48;
+
+        public int DownloadImageFromWebAndAddToImageList(string imageUrl, ImageList avatarImageList)
+        {
+            if (avatarImageList.Images.ContainsKey(imageUrl))
+                return avatarImageList.Images.IndexOfKey(imageUrl);
+
+            int avatarId;
+            var webClient = new WebClient();
+            byte[] imageRaw = webClient.DownloadData(imageUrl);
+            using (MemoryStream gravatarMs = new MemoryStream(imageRaw))
+            {
+                Image gravatarImage = Image.FromStream(gravatarMs);
+                avatarImageList.Images.Add(imageUrl, gravatarImage);
+                avatarId = avatarImageList.Images.Count - 1;
+            }
+            return avatarId;
+        }
+
+        public int DownloadGravatarFromEmailAndAddToImageList(string email, ImageList avatarImageList)
+        {
+            var imgSrc = GetImgSrc(email, DEFAULT_IMAGE_SIZE);
+            return DownloadImageFromWebAndAddToImageList(imgSrc, avatarImageList);
+        }
+
+        public static string GetImgSrc(string email, int size)
+        {
+            string hash = GetMd5Hash(email);
+            var rand = new Random(string.IsNullOrEmpty(email) ? 0 : email[0]).Next(1, 8);
+            return string.Format("http://www.gravatar.com/avatar/{0}?s={1}&d=http%3A%2F%2Fsirenofshame.com%2FImages%2Fdude{2}.jpg",
+                hash,
+                size,
+                rand);
+        }
+
+        private static string GetMd5Hash(string input)
+        {
+            if (string.IsNullOrEmpty(input)) return "";
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            byte[] bs = Encoding.UTF8.GetBytes(input);
+            bs = md5.ComputeHash(bs);
+            StringBuilder s = new StringBuilder();
+            foreach (byte b in bs)
+            {
+                s.Append(b.ToString("x2").ToLower());
+            }
+            string password = s.ToString();
+            return password;
+        }
+    }
+}
---
+++ SirenOfShame.Lib/Services/ReputationResetService.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using log4net;
+using SirenOfShame.Lib.Settings;
+using SirenOfShame.Lib.Watcher;
+
+namespace SirenOfShame.Lib.Services
+{
+    public class ReputationResetService
+    {
+        private readonly SirenOfShameSettings _settings;
+        private readonly SosDb _sosDb = new SosDb();
+        private readonly ILog _log = MyLogManager.GetLogger(typeof (ReputationResetService));
+
+        public ReputationResetService(SirenOfShameSettings settings)
+        {
+            _settings = settings;
+        }
+
+        public void ResetOnly()
+        {
+            _settings.Backup();
+            foreach (var person in _settings.People)
+            {
+                person.TotalBuilds = 0;
+                person.FailedBuilds = 0;
+                person.Achievements = new List<AchievementSetting>();
+                person.CumulativeBuildTime = 0;
+                person.CurrentBuildRatio = 0;
+                person.CurrentSuccessInARow = 0;
+                person.LowestBuildRatioAfter50Builds = 0;
+                person.MaxBuildsInOneDay = 0;
+                person.NumberOfTimesFixedSomeoneElsesBuild = 0;
+                person.NumberOfTimesPerformedBackToBackBuilds = 0;
+            }
+            _settings.Save();
+
+        }
+
+        public void RebuildSince(DateTime? since, Action<string> reportProgress)
+        {
+            try
+            {
+                var buildStatuses = _sosDb.ReadAll();
+                var sortedBuildStatuses = buildStatuses
+                    .Where(buildStatus => since == null || buildStatus.StartedTime > since)
+                    .OrderByDescending(buildStatus => buildStatus.LocalStartTime)
+                    .ToList();
+
+                var totalBuilds = sortedBuildStatuses.Count;
+
+                var rulesEngine = new RulesEngine(_settings)
+                {
+                    DisableSosOnline = true,
+                    DisableWritingToSosDb = true
+                };
+                int i = 0;
+                foreach (var buildStatus in sortedBuildStatuses)
+                {
+                    rulesEngine.ExecuteNewBuilds(new List<BuildStatus> {buildStatus});
+                    if (i%10 == 0)
+                    {
+                        reportProgress(string.Format("Processing {0}/{1}", i, totalBuilds));
+                    }
+                    i++;
+                }
+                _settings.Save();
+                reportProgress("Completed reset successfully");
+            }
+            catch (Exception ex)
+            {
+                _log.Error("Error in sync", ex);
+                reportProgress("Error, check the logs. " + ex.Message);
+            }
+        }
+    }
+}
